a3282055a8cfab4812f9fdbdd279f245ad0a2b2f
Samuele Carcagno <sam.carcagno@gmail.com>
2018-07-24 11:30:48 +0100

    fix example

--- buildCheck.sh
+++ buildCheck.sh
@@ -3,6 +3,6 @@
 R CMD build ggsam/
 rm ggsam.pdf
 R CMD Rd2pdf ggsam/
-R CMD check ggsam_1.0.6.tar.gz
+R CMD check ggsam_1.0.7.tar.gz
 
 
--- ggsam/R/theme_bwng.R
+++ ggsam/R/theme_bwng.R
@@ -1,7 +1,6 @@
 ##
 ## black and white theme without grid
-theme_bwng = function(base_size = 12, base_family = "", base_line_size = base_size/22, 
-    base_rect_size = base_size/22){
+theme_bwng = function(base_size = 12, base_family = "", base_line_size = base_size/22, base_rect_size = base_size/22){
     theme_bw(base_size = base_size, base_family = base_family) %+replace%
         theme(panel.grid.major = element_blank(),
               panel.grid.minor = element_blank(),
