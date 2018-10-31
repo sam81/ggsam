71c581796b5af0b2964448c9b543c6d0fb457f21
Samuele Carcagno <sam.carcagno@gmail.com>
2018-10-31 00:43:22 +0000

    fix theme_bwng axis issue

--- ggsam/R/theme_bwng.R
+++ ggsam/R/theme_bwng.R
@@ -1,9 +1,9 @@
 ##
 ## black and white theme without grid
 theme_bwng = function(base_size = 12, base_family = "", base_line_size = base_size/22, base_rect_size = base_size/22){
-    theme_bw(base_size = base_size, base_family = base_family) %+replace%
+    theme_bw(base_size = base_size, base_family = base_family, base_line_size = base_line_size, base_rect_size = base_rect_size) %+replace%
         theme(panel.grid.major = element_blank(),
               panel.grid.minor = element_blank(),
-              axis.text.x = element_text(colour = "black"),
-              axis.text.y = element_text(colour = "black"))
+              axis.text = element_text(colour = "black"))
 }
+
